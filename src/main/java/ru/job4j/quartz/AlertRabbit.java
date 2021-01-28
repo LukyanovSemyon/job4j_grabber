fe5abf3ef554f59bfe4b199353973d79a5f970f0
LukyanovSemyon <lukyakha@gmail.com>
2021-01-28 18:17:05 +0300

    1.1. Job c параметрами[260360#387640]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -12,7 +13,15 @@ import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    private Connection connection;
+    private final Properties properties;
+
+    public AlertRabbit(Properties properties) throws SQLException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private static Properties getProperties() {
         Properties prs = new Properties();
         try (InputStream io = AlertRabbit.class.getClassLoader()
                 .getResourceAsStream("rabbit.properties")) {
@@ -20,11 +29,33 @@ public class AlertRabbit {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        int rabbitInterval = Integer.parseInt(prs.getProperty("rabbit.interval"));
+        return prs;
+    }
+
+    private void initConnection() throws SQLException {
+        String url = properties.getProperty("postgresql.url");
+        String login = properties.getProperty("postgresql.login");
+        String password = properties.getProperty("postgresql.password");
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public void job() {
         try {
+            createTable();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
+            int rabbitInterval = Integer.parseInt(getProperties().getProperty("rabbit.interval"));
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(rabbitInterval)
                     .repeatForever();
@@ -33,15 +64,45 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(connection);
+            close();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
+    public void createTable() throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            String sql;
+            sql = "create table if not exists rabbit(id serial primary key, created_date long);";
+            statement.execute(sql);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        AlertRabbit rabbit = new AlertRabbit(getProperties());
+        rabbit.job();
+    }
+
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection s = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement statement = s
+                    .prepareStatement("insert into rabbit(created_date) values(?)")) {
+                statement.setLong(1, System.currentTimeMillis());
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
