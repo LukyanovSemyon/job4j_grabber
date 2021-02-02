87d99931b2a2bb96a45d031c5cee75b897d601f4
LukyanovSemyon <lukyakha@gmail.com>
2021-02-02 19:05:18 +0300

    2.4. SqlRuParse[285213#387645]

--- src/main/java/ru/job4j/html/Post.java
+++ src/main/java/ru/job4j/html/Post.java
@@ -3,15 +3,23 @@ package ru.job4j.html;
 import java.util.Date;
 
 public class Post {
+    private final String title;
+    private final String link;
     private final String description;
     private final Date createdDate;
 
-    public Post(String description, Date createdDate) {
+    public Post(String title, String description, String link, Date createdDate) {
+        this.title = title;
         this.description = description;
+        this.link = link;
         this.createdDate = createdDate;
     }
 
-    public String getMsgBody() {
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
         return description;
     }
 
@@ -19,10 +27,16 @@ public class Post {
         return createdDate;
     }
 
+    public String getLink() {
+        return link;
+    }
+
     @Override
     public String toString() {
-        return "Post{"
-                + "description='" + description + '\''
-                + ", createdDate=" + createdDate + '}';
+        return "Post{" + "title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", createdDate=" + createdDate + '}'
+                + System.lineSeparator();
     }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,37 +6,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-            Elements row = doc.select("tr");
-            for (Element td : row) {
-                Elements postslisttopic = td.select(".postslisttopic");
-                for (Element a : postslisttopic) {
-                    Element href = a.child(0);
-                    String date = td.child(5).text();
-                    System.out.println(href.attr("href"));
-                    System.out.println(href.text());
-                    System.out.println(date(date));
-                }
-            }
-        }
-        Post post = post("https://www.sql.ru/forum/1325330"
-                + "/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
-        System.out.println(post);
+public class SqlRuParse implements Parse {
+    public SqlRuParse() {
     }
 
-    public static Post post(String a) throws IOException {
-        Document doc = Jsoup.connect(a).get();
-        Elements msgBody = doc.select(".msgBody");
-        Elements msgFooter = doc.select(".msgFooter");
-        Element body = msgBody.get(1);
-        String date = msgFooter.get(0).text().split("\\[")[0];
-        return new Post(body.text(), date(date));
+    public static void main(String[] args) throws IOException {
+        System.out.println(new SqlRuParse().list("https://www.sql.ru/forum/job-offers/"));
     }
 
     public static Date date(String date) {
@@ -98,5 +78,31 @@ public class SqlRuParse {
         }
         calendar.set(year, month, day, hour, minute);
         return calendar.getTime();
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> list = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect(link + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                list.add(detail(href.attr("href")));
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public Post detail(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Elements messageHeader = doc.select(".messageHeader");
+        Elements msgBody = doc.select(".msgBody");
+        Elements msgFooter = doc.select(".msgFooter");
+        Element title = messageHeader.get(0);
+        Element body = msgBody.get(1);
+        String date = msgFooter.get(0).text().split("\\[")[0];
+        return new Post(title.text(), body.text(), link, date(date));
     }
 }
