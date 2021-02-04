4e643bf30f43dcc5384aabdc4dd31eb9d48f7f4b
LukyanovSemyon <lukyakha@gmail.com>
2021-02-04 23:23:15 +0300

    2.1. Преобразование даты[289476#387646] Correction#2

--- src/main/java/ru/job4j/grabber/DateParse.java
+++ src/main/java/ru/job4j/grabber/DateParse.java
@@ -6,6 +6,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DateParse {
+    private static final Map<String, Integer> MONTHS = new HashMap<>() {{
+        put("янв", Calendar.JANUARY);
+        put("фев", Calendar.FEBRUARY);
+        put("мар", Calendar.MARCH);
+        put("апр", Calendar.APRIL);
+        put("май", Calendar.MAY);
+        put("июн", Calendar.JUNE);
+        put("июл", Calendar.JULY);
+        put("авг", Calendar.AUGUST);
+        put("сен", Calendar.SEPTEMBER);
+        put("окт", Calendar.OCTOBER);
+        put("ноя", Calendar.NOVEMBER);
+        put("дек", Calendar.DECEMBER);
+    }};
+
     public static Date date(String date) {
         String num = date.trim().split(",")[0];
         String time = date.split(",")[1];
@@ -15,19 +30,6 @@ public class DateParse {
         int day;
         int month = calendar.get(Calendar.MONTH);
         int year = calendar.get(Calendar.YEAR);
-        Map<String, Integer> mon = new HashMap();
-        mon.put("янв", Calendar.JANUARY);
-        mon.put("фев", Calendar.FEBRUARY);
-        mon.put("мар", Calendar.MARCH);
-        mon.put("апр", Calendar.APRIL);
-        mon.put("май", Calendar.MAY);
-        mon.put("июн", Calendar.JUNE);
-        mon.put("июл", Calendar.JULY);
-        mon.put("авг", Calendar.AUGUST);
-        mon.put("сен", Calendar.SEPTEMBER);
-        mon.put("окт", Calendar.OCTOBER);
-        mon.put("ноя", Calendar.NOVEMBER);
-        mon.put("дек", Calendar.DECEMBER);
         if (num.equals("сегодня")) {
             day = calendar.get(Calendar.DATE);
         } else if (num.equals("вчера")) {
@@ -35,7 +37,7 @@ public class DateParse {
         } else {
             day = Integer.parseInt(num.split(" ")[0]);
             String mm = num.trim().split(" ")[1];
-            month = mon.get(mm);
+            month = MONTHS.get(mm);
             year = Integer.parseInt(num.split(" ")[2]) + 2000;
         }
         calendar.set(year, month, day, hour, minute);
